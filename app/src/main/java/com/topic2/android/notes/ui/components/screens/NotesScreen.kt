6a4552d6c1073fb694d92bbd78df4ccfbf9471f3
Anastasia <3778242nastya@gmail.com>
2024-03-28 23:01:40 +0300

    created panel app

--- app/src/main/java/com/topic2/android/notes/ui/components/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screens/NotesScreen.kt
@@ -1,33 +1,58 @@
 package com.topic2.android.notes.ui.components.screens
 
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.ui.components.AppDrawer
 import com.topic2.android.notes.ui.components.Note
 import com.topic2.android.notes.ui.components.TopAppBar
 import com.topic2.android.notes.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun NotesScreen(
-    viewModel: MainViewModel
+    viewModel: MainViewModel,
+
 ){
 val notes: List<NoteModel> by viewModel
     .notesNotInTrash
     .observeAsState(listOf())
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val  coroutineScope: CoroutineScope= rememberCoroutineScope()
     Scaffold (
         topBar = {
             TopAppBar(
                 title ="Notes" ,
-                icon = Icons.Filled.List, onIconClick = {})
-
+                icon = Icons.Filled.List,
+                onIconClick = {
+                    coroutineScope.launch{
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
                  },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+                        AppDrawer(currentScreen = Screen.Notes, closeDrawerAction = {
+                            coroutineScope.launch{
+                                scaffoldState.drawerState.close()
+                            }
+                        }
+                        )
+                          },
         content = {
        if (notes.isNotEmpty()) {
            NotesList(
