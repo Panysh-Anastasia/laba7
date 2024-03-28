94f98bea64e4c0a87c3d42d3116f5848e8211780
Anastasia <3778242nastya@gmail.com>
2024-03-28 21:59:45 +0300

    laba 7

---
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.topic2.android.notes
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
+import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.ui.components.screens.NotesScreen
+import com.topic2.android.notes.viewmodel.MainViewModel
+import com.topic2.android.notes.viewmodel.MainViewModelFactory
+
+/**
+ * Main activity приложения.
+ */
+class MainActivity : AppCompatActivity() {
+
+  private val viewModel: MainViewModel by viewModels(factoryProducer = {
+    MainViewModelFactory(
+      this,
+      (application as NotesApplication).dependencyInjector.repository
+    )
+  })
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContent {
+      NotesTheme {
+        NotesScreen(viewModel = viewModel)
+
+
+      }
+    }
+  }
+}
+
